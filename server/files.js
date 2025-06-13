a5ee5465a75d834da6df799af35601c949c025a7
gurpreetk05 <60051372+gurpreetk05@users.noreply.github.com>
2025-06-13 12:59:02 -0600

    Update files.js

--- server/files.js
+++ server/files.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const multer = require('multer'); // used for uploading files
+const {uploadfile, downloadfile} =require('../encryptdecrpyt/filesencrpytdecrpyt');
+
+//for better organization
+const router = express.Router();
+const upload = multer({dest:"uploads/"}); 
+
+//defining routes
+router.post('/upload'. upload.single('file'), uploadfile);
+router.get('/download/:filename', downloadfile);
+module.exports=router;
+
 
