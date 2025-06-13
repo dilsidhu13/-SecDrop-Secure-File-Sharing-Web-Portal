a39e60209a5a3f0722058bd33d5b34c0999205c7
gurpreetk05 <60051372+gurpreetk05@users.noreply.github.com>
2025-06-13 12:30:10 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,19 @@
 const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
+const dontenv= require('dontenv');
+const fileroutes = require('./routes/files');
+const adminroutes= require('./routes/admin');
+
+dontenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("uploads"));
+app.use('/api/files', fileroutes);
+app.use('/api/admin', adminroutes);
+
 
 app.get('/', (req, res) => res.send('SecDrop backend is running!'));
 
