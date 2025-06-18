fa2cf836dada0df1314daa1da89115267391bd8a
dilsidhu13 <d.sidhu6461@gmail.com>
2025-06-18 14:24:26 -0600

    Rewind

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 // src/App.js
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import LandingPage from './LandingPage.js';
 import FileEncryptUpload from './FileEncryptUpload.js';
-import P2PFileTransfer from './P2PFileTransfer.js';
 import logo from './logo.webp';   // logo Photo
 import './App.css';
 
@@ -21,18 +18,8 @@ function App() {
         </div>
       </header>
 
-      <nav style={{ margin: '1rem' }}>
-        <Link to="/upload">Encrypted Upload</Link>
-        <Link to="/p2p" style={{ marginLeft: '1rem' }}>P2P Transfer</Link>
-      </nav>
-
       <main>
-// 
- <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/upload" element={<FileEncryptUpload />} />
-          <Route path="/p2p" element={<P2PFileTransfer />} />
-        </Routes>
+        <FileEncryptUpload />
       </main>
     </div>
   );
