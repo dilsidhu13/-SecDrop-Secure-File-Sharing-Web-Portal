f27bbce1d96339d38c4c4adc06341dbf1e62506b
dilsidhu13 <d.sidhu6461@gmail.com>
2025-06-18 15:03:32 -0600

    Updated

--- server/server.js
+++ server/server.js
@@ -219,4 +219,4 @@ app.get('/api/download', async (req, res) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`🚀 SecDrop backend listening on ${port}`));
+app.listen(port, () => console.log(`SecDrop backend listening on ${port}`));
