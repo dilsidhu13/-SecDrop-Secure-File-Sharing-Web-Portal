e59856de7a64d4a327000fd9a3a88f145e005a64
gurpreetk05 <60051372+gurpreetk05@users.noreply.github.com>
2025-06-23 15:54:12 -0600

    Update admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const { loginAdmin } = require("../encryptdecrypt/adminencryptdecrypt");
 
+const router = express.Router();
+
+// Routes
+router.post("/login", loginAdmin);
+
+module.exports = router;
