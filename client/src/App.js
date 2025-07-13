eedfbbafb920b918968e4fcb6e6af1070e451438
gurpreetk05 <60051372+gurpreetk05@users.noreply.github.com>
2025-07-13 15:59:50 -0600

    Updating Frontend and UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
 import Homepage from './Homepage';
+import About from './About';
+//import p2pTransfer from './P2PTransfer'; // Assuming you have a P2PTransfer component
 import FileEncryptUpload from './FileEncryptUpload';
 import DownloadPage from './DownloadPage';
 import './App.css';
@@ -8,10 +12,12 @@ import './App.css';
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/upload" element={<FileEncryptUpload />} />
         <Route path="/download/:id" element={<DownloadPage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
